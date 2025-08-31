d9111b7e2e703b784527aeb351b1da2dd2af77ab
owTnitraM <martin.n2bt@gmail.com>
2025-08-30 22:55:45 -0300

    grafar intro

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from tabelas import generate_tables
 def main():
     seed_input: int = int(input("Escolha a seed q deseja usar: "))
     print(generate_tables(seed=seed_input))
+    
     user_choice: str | None = None
+    
     while user_choice != "s":
         user_choice: str = (input("[c] Criptografar | [d] Descriptografar | [s] Sair : ")).lower()
         if user_choice == "c":
-            grafar(input("texto a grafar: "))
+            print(grafar(input("texto a grafar: ")))
         elif user_choice == "d":
             user_input: str = input("Texto a desgrafar: ")
             chaves: list[int] | str = input("Enter the key separated by commas (ex: 7, 8, 9): ")
