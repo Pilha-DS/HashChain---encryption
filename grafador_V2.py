b9ef55d126821c3c22387e0c7af7ecbfc2f7a221
Pilha-DS <jonathansanchescardoso@gmail.com>
2025-08-31 00:46:38 -0300

    Update grafador_V2.py
    update

--- grafador_V2.py
+++ grafador_V2.py
@@ -1,4 +1,4 @@
-chaves = {
+tabelas = {
     7: {
         "a": "#*#####",
         "A": "##*####",
@@ -96,46 +96,75 @@ chaves = {
     },
 }
 
-#funcoes
-def pegar_chave_por_index(dicionario, indice):
-    return list(dicionario.keys())[indice]
+#Funcões
+def grafar(text_to_crip: str, passes: [int] = [0]):
 
-def pegar_chave_por_valor(dicionario, valor):
-    for chave, val in dicionario.items():
-        if val == valor:
-            return chave
- 
-def grafar(text_to_crip: str, the_pass):
-    text_to_crip
-    the_pass
+    def pegar_chave_por_index(dicionario, indice):
+        return list(dicionario.keys())[indice]
+
+    def pegar_chave_por_valor(dicionario, valor):
+        for chave, val in dicionario.items():
+            if val == valor:
+                return chave
+            
+    def limpar_grifo(texto, manter):
+        return "".join([c for c in texto if c in manter])
+
+    the_pass = tabelas
     cripted_text = ""
     caracteres_invalidos = ""
-    keyboard = (len(the_pass) - 1)
-    print (f"keyboard: {keyboard}")
+    passes_usados = ""
     x = 0
-                 
-    for t in text_to_crip:
-        if x > keyboard:
-            x = 0    
-            if t in the_pass[pegar_chave_por_index(the_pass, x)]:
-                print(f"x1: {x}")
-                cripted_text = cripted_text + " " +the_pass[pegar_chave_por_index(the_pass, x)][t]
+    
+    if passes != [0]:
+    #Mandando o passe.
+        xx = 0
+        for t in text_to_crip:
+            if xx == passes[-1]:
+                x = 0
+                xx = passes[x]
+                if t in the_pass[xx]:
+                    passes_usados = passes_usados + str(passes[x]) + "," + " "
+                    cripted_text = cripted_text + the_pass[xx][t]
+                    x += 1
+                else:
+                    caracteres_invalidos = caracteres_invalidos + t + "," + " "
             else:
-                caracteres_invalidos = caracteres_invalidos + t + "," + " "
-            x += 1
-                
+                xx = passes[x]
+                if t in the_pass[xx]:
+                    passes_usados = passes_usados + str(passes[x]) + "," + " "
+                    cripted_text = cripted_text + the_pass[xx][t]
+                    x += 1
+                else:
+                    caracteres_invalidos = caracteres_invalidos + t + "," + " "
         else:
-            if t in the_pass[pegar_chave_por_index(the_pass, x)]:
-                print(f"x2: {x}")
-                cripted_text = cripted_text + " " +the_pass[pegar_chave_por_index(the_pass, x)][t]
-            else:
-                caracteres_invalidos = caracteres_invalidos + t + "," + " "
-            x += 1
-
+            return f"Texto grafado: {text_to_crip} : Grafo: {cripted_text} : Caracteres invalídos ({caracteres_invalidos}) : Os passes usados são: ({passes_usados})"
+        
+    #Não mandando o passe.
     else:
-        return f"Texto grafado: {text_to_crip} : Grafo: {cripted_text} : Caracteres invalídos ({caracteres_invalidos})"
+        keyboard = (len(the_pass))
+        print (f"keyboard: {keyboard}")
+        for t in text_to_crip:
+            if x == keyboard:
+                x = 0    
+                if t in the_pass[pegar_chave_por_index(the_pass, x)]:
+                    passes_usados = passes_usados + str(pegar_chave_por_index(the_pass, x)) + "," + " "
+                    cripted_text = cripted_text + the_pass[pegar_chave_por_index(the_pass, x)][t]
+                else:
+                    caracteres_invalidos = caracteres_invalidos + t + "," + " "
+                x += 1  
+            else:
+                if t in the_pass[pegar_chave_por_index(the_pass, x)]:
+                    passes_usados = passes_usados + str(pegar_chave_por_index(the_pass, x)) + "," + " "
+                    cripted_text = cripted_text + the_pass[pegar_chave_por_index(the_pass, x)][t]
+                else:
+                    caracteres_invalidos = caracteres_invalidos + t + "," + " "
+                x += 1
+        else:
+            cripted_text = limpar_grifo(cripted_text, "#*")
+            return f"Texto grafado: {text_to_crip} : Grafo: {cripted_text} : Caracteres invalídos ({caracteres_invalidos}) : Os passes usados são: ({passes_usados})"
         
 #fluxo
 while True:
     text_t = input("Escreva oque quer criptografar: ")
-    print(grafar(text_t, chaves))
+    print(grafar(text_t))
