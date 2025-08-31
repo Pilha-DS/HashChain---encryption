93f4d688e002fb334ee9c2e5fb1ee39b9716be3f
owTnitraM <martin.n2bt@gmail.com>
2025-08-31 01:43:17 -0300

    aa

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-for i in range(9, 25):
-        for char in all_chars:
-            while tables[i].keys() == "":
-                
-                
-            tables[i][char] = "#" + char_grafado + "#"
-            char_grafado = ""
