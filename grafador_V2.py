9714c8bf857c2ceb6cd7625bf2cf2a6ce592daae
Pilha-DS <jonathansanchescardoso@gmail.com>
2025-08-30 22:09:30 -0300

    grafador_novo

--- grafador_V2.py
+++ grafador_V2.py
@@ -1,51 +1,50 @@
 chaves = {
     7: {
         "a": "#*#####",
-        "A": "#####",
-        "b": "##*####",
-        "B": "#####",
-        "c": "###*###",
-        "C": "#####",
-        "d": "####*##",
-        "D": "#####",
-        "e": "#####*#",
-        "E": "#####*#",
-        "f": "###*#",
-        "F": "###",
-        "g": "###",
-        "G": "#*#",
+        "A": "##*####",
+        "b": "###*###",
+        "B": "####*##",
+        "c": "#####*#",
+        "C": "#**####",
+        "d": "##**###",
+        "D": "###**##",
+        "e": "####**#",
+        "E": "#*#*###",
+        "f": "###*#*#",
+        "F": "##*#*##",
+        "g": "#*##*##",
     },
     8: {
         "a": "#*######",
-        "A": "######",
-        "b": "##*#####",
-        "B": "######",
-        "c": "###*####",
-        "C": "######",
-        "d": "####*###",
-        "D": "######",
-        "e": "#####*##",
-        "E": "######",
-        "f": "######*#",
-        "F": "######",
-        "g": "######",
-        "G": "####*#",
+        "A": "##*#####",
+        "b": "###*####",
+        "B": "####*###",
+        "c": "#####*##",
+        "C": "######*#",
+        "d": "#**#####",
+        "D": "##**####",
+        "e": "###**###",
+        "E": "####**##",
+        "f": "#####**#",
+        "F": "#*#*####",
+        "g": "##*#*###",
+        "G": "###*#*##",
     },
     9: {
         "a": "#*#######",
-        "A": "#######",
-        "b": "##*######",
-        "B": "#######",
-        "c": "###*#####",
-        "C": "#######",
-        "d": "####*####",
-        "D": "#######",
-        "e": "#####*###",
-        "E": "#######",
-        "f": "######*##",
-        "F": "#######",
-        "g": "#######*#",
-        "G": "#######",
+        "A": "##*######",
+        "b": "###*#####",
+        "B": "####*####",
+        "c": "#####*###",
+        "C": "######*##",
+        "d": "#######*#",
+        "D": "#**######",
+        "e": "##**#####",
+        "E": "###**####",
+        "f": "####**###",
+        "F": "#####**##",
+        "g": "######**#",
+        "G": "#*#*#####",
     },
 }
 
@@ -62,28 +61,37 @@ def grafar(text_to_crip: str, the_pass):
     text_to_crip
     the_pass
     cripted_text = ""
+    caracteres_invalidos = ""
     keyboard = len(the_pass) - 1
+    print (f"keyboard: {keyboard}")
     x = 0
                  
     for t in text_to_crip:
         if x > keyboard:
-            x = 0
-            print(f"x1: {x}")
-            cripted_text = cripted_text + pegar_chave_por_valor(the_pass[x], t)
+            x = 0    
+            if t in the_pass[pegar_chave_por_index(the_pass, x)]:
+                print(f"x1: {x}")
+                cripted_text = cripted_text + " " +the_pass[pegar_chave_por_index(the_pass, x)][t]
+            else:
+                caracteres_invalidos = caracteres_invalidos + t + "," + " "
             x += 1
-        else:
-            print(the_pass[pegar_chave_por_index(the_pass, x)][t])
-            """ if t in the_pass[pegar_chave_por_index(the_pass, x)]:
-                cripted_text = cripted_text + pegar_chave_por_valor(the_pass[x], t)
-                x += 1 """
                 
+        else:
+            if t in the_pass[pegar_chave_por_index(the_pass, x)]:
+                print(f"x2: {x}")
+                cripted_text = cripted_text + " " +the_pass[pegar_chave_por_index(the_pass, x)][t]
+            else:
+                caracteres_invalidos = caracteres_invalidos + t + "," + " "
+            x += 1
+
+    else:
+        return f"Texto grafado: {text_to_crip} : Grafo: {cripted_text} : Caracteres invalídos ({caracteres_invalidos})"
+        
 #fluxo
+while True:
+    text_t = input("Escreva oque quer criptografar: ")
+    print(grafar(text_t, chaves))
 
-""" valor = input("a: ")
-print(inverter_dicionario(pass_s[0], valor)) """
-
-text_t = input("Escreva oque quer criptografar: ")
-print(grafar(text_t, chaves))
 
 
 
