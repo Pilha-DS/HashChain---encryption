47ac7d43001c187c5e958f54040e1b2d32d061a0
Vinicius Andriato Manini <150202425+VaManini@users.noreply.github.com>
2025-09-28 18:49:11 -0300

    Update descompactador.py
    Mudei a descompactação para usar com números, então vai ser o valor binário

--- APS/EmTeste/descompactador.py
+++ APS/EmTeste/descompactador.py
@@ -1,21 +1,18 @@
-norm = "4#5*#*#*4#3*#2*3#3*"
+norm = "1010107150510160103130"
 textF = ""
 numb = 0
+            
+for posicao in norm:
+    
+    if numb + 1 < len(norm) and norm[numb + 1] != posicao:
+        if posicao != "0" and posicao != "1":
+            textF += (int(posicao) - 1) * norm[numb + 1]
+        else:    
+            textF += posicao
+    if numb + 1 == len(norm):
+        textF += norm[-1]
 
-for posicao, i in enumerate(norm):
-    if i.isdigit():
-        numb = int(str(numb) + i) if numb else int(i)
-
-        if posicao + 1 < len(norm) and norm[posicao + 1].isdigit():
-            continue
-
-        if posicao + 1 < len(norm):
-            textF += norm[posicao + 1] * (numb - 1)
-
-        numb = 0
-    else:
-        if numb == 0:
-            textF += i
-
+    numb += 1
+    
 print(textF)
             
