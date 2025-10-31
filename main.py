fd45c6007a12bc5ed2403584907b74e29a882528
Pilha-DS <jonathansanchescardoso@gmail.com>
2025-10-31 02:07:35 -0300

    Atualização insiginificante
    seomente para testes do novo HCC

--- main.py
+++ main.py
@@ -9,18 +9,11 @@ HashChain: HashChainEncryption = HashChainEncryption()
 # Will handle user input, function calls and the interface (WiP).
 def main():
     H = HashChainEncryption()
-    plaintext = "aba"
-    for i in range(10):
-        H.encrypt_(plaintext)
-        dec = H.decrypt_(H.info(0), H.info(1))
-        if dec != plaintext:
-            print(f"FAIL at {i}: dec={dec!r}")
-            return
-        print(f"OK at {i}: dec={dec!r}")
-    print("OK 200 runs")
 
     H.encrypt_("ovolate")
-    print(H.decrypt_(H._info[0], H._info[1]))
+
+    H.encrypt_("abacate", no_salt=True)
+
 # Runs main the function if the "main.py" file is directly executed.
 if __name__ == "__main__":
     main()
